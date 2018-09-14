8f848bab4128cc153b992d52f5130a26376e85eb
Samuel Ware <sware@snares-08.cs.wisc.edu>
2018-09-13 19:27:27 -0500

    Add all the files needed

---
+++ getprocs.h
@@ -0,0 +1,8 @@
+#ifndef GETPROCS_H
+#define GETPROCS_H
+
+char** getproclist();
+
+char* getproc(char* pid);
+
+#endif
---
+++ statparse.h
@@ -0,0 +1,12 @@
+#ifndef STATPARSE_H
+#define STATPARSE_H
+
+char getstate();
+
+char* getutime();
+
+char* getstime();
+
+char* getvmem();
+
+#endif
