9e971d531d69bff0a516ea3196e3cc30652c10fa
loganpmahan <loganpearcemahan@gmail.com>
2018-09-22 12:25:47 -0500

    Update getprocs.h

--- getprocs.h
+++ getprocs.h
@@ -1,7 +1,12 @@
 #ifndef GETPROCS_H
 #define GETPROCS_H
 
-char** getproclist();
+typedef struct Procnode{
+    char* pid;
+    Procnode* next;
+}
+
+Procnode* getproclist();
 
 char* getproc(char* pid);
 
